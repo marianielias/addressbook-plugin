23ebb3630c8cd039fba33d4fc5df8cca1fbf9579
FlingeR <marianielias@gmail.com>
2016-02-25 12:59:03 -0300

    redirect if the api_key is not defined

--- controllers/People.php
+++ controllers/People.php
@@ -3,8 +3,10 @@
 use BackendMenu;
 use Backend\Classes\Controller;
 use Fencus\AddressBook\Models\Person as PersonModel;
+use Fencus\GoogleMapsWidgets\Models\Settings as MapsSettings;
 use Flash;
 use Lang;
+use Backend;
 
 /**
  * People Back-end Controller
@@ -19,11 +21,39 @@ class People extends Controller
     public $formConfig = 'config_form.yaml';
     public $listConfig = 'config_list.yaml';
 
+    public $api_key;
+    
     public function __construct()
     {
+    	$this->api_key = MapsSettings::get('api_key');
+    	
         parent::__construct();
 
         BackendMenu::setContext('Fencus.AddressBook', 'addressbook', 'people');
+    }
+    
+    public function index()
+    {
+    	if(!$this->api_key)
+    		return Redirect(Backend::url('system/settings/update/fencus/googlemapswidgets/settings'));
+    
+    		$this->asExtension('ListController')->index();
+    }
+    
+    public function create()
+    {
+    	if(!$this->api_key)
+    		return Redirect(Backend::url('system/settings/update/fencus/googlemapswidgets/settings'));
+    
+    		return $this->asExtension('FormController')->create();
+    }
+    
+    public function update($recordId = null)
+    {
+    	if(!$this->api_key)
+    		return Redirect(Backend::url('system/settings/update/fencus/googlemapswidgets/settings'));
+    
+    		return $this->asExtension('FormController')->update($recordId);
     }
     
     public function index_onDelete()
